8fa88c475ca38525bf289245c90b857716f47242
C:/Users/user <C:/Users/user>
2022-08-20 22:45:19 -0300

    teste

--- CSharpBasico/CSharpBasico/Program.cs
+++ CSharpBasico/CSharpBasico/Program.cs
@@ -12,6 +12,25 @@
 //Console.WriteLine("___Idade: " + idade + "____");
 //Console.WriteLine("___________________________");
 
+//int teste = 23;
+//SByte teste2 = -23;
 
+//Console.WriteLine(teste);
+//Console.WriteLine(teste2);
 
+Console.Write("Aluno ");
+String Aluno = Console.ReadLine();
+
+Console.Write("Nota 1");
+Int32 nota1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nota 2");
+Int32 nota2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Nota 3");
+Int32 nota3 = Convert.ToInt32(Console.ReadLine());
+
+Int32 media = (nota1 + nota2 + nota3) / 3;
+
+Console.WriteLine(media);
 Console.ReadLine();
