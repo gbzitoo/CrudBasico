94b2c7235e57e549096f5bcb59676642b89e0634
Gabriel Oliveira <gariel_moliveira@sicredi.com.br>
2022-08-20 21:06:51 -0300

    Pratica 1

--- CSharpBasico/CSharpBasico/Program.cs
+++ CSharpBasico/CSharpBasico/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//string name;
+//int idade;
+
+//Console.Write("Name: ");
+//name = Console.ReadLine();
+
+//Console.Write("Idade: ");
+//idade = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("___________________________");
+//Console.WriteLine("______Nome: "+name+"_______");
+//Console.WriteLine("___Idade: " + idade + "____");
+//Console.WriteLine("___________________________");
+
+
+
+Console.ReadLine();
